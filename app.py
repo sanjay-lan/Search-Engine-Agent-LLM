84c8120a2b9dcb8163fa3c244b8ae8a5c73aac1e
Sanjoy Goswami <63977590+sanjay-lan@users.noreply.github.com>
2025-04-28 07:50:52 +0530

    1st commit
    search engine powered by ai agents of Wikipedia, Arxiv and Duckduckgo search

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun, WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain.agents import initialize_agent,AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+import os
+
+
+# Arxiv and wikipedia tolls
+arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+api_wrapper=WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
+
+search = DuckDuckGoSearchRun(name="Search")
+
+st.title("Langchain - chat with search")
+
+# st.sidebar.title("Settings")
+# api_key = st.sidebar.text_input("Enter your Groq API key: ", type = "password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[
+        { 
+            "role":"assistent", "content":"Hi, I'm a chatbot who can search the web. How can i help you?"
+        }
+    ]
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if prompt:=st.chat_input(placeholder="What is machine learning?"):
+    st.session_state.messages.append({"role":"user","content":prompt})
+    st.chat_message("user").write(prompt)
+
+    llm = ChatGroq(model_name="Gemma2-9b-It", streaming=True)
+    tools=[search,arxiv,wiki]
+    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+
+    with st.chat_message("assistant"):
+        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=True)
+        response= search_agent.run(st.session_state.messages,callbacks=[st_cb])
+        st.session_state.messages.append({'role':'assistant',"content":response})
+        st.write(response)
+
